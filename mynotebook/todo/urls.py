792a473e1f024981c09ddae934a29f32478b707e
NataliiaIgra <natasha_igra@mail.ru>
2021-04-14 11:33:02 +0300

    Some updates

--- mynotebook/todo/urls.py
+++ mynotebook/todo/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import NoteListView, NoteView, NoteEditView
+from .views import NoteListView, NoteView, NoteEditView, AboutView
 
-# Для ViewSet
-router = DefaultRouter()
-router.register(r'trial', views.NoteListViewSet, basename='trial')
-
-app_name = 'todo'
 urlpatterns = [
     path('note/', NoteListView.as_view()),
-    path('note/<int:note_id>', NoteView.as_view()),
-    path('note/add/', NoteEditView.as_view()),
-    path('', include(router.urls))
+    # path('note/<int:note_id>', NoteView.as_view()),
+    # path('note/add/', NoteEditView.as_view()),
+    # path('', include(router.urls)),
+    path('about/', AboutView.as_view()),
 ]
